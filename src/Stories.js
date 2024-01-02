48330d45ae95b17fa31de4ce2384164146cec0f6
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2024-01-02 11:44:01 +0500

    251 - Hacker News Remove Story

--- src/Stories.js
+++ src/Stories.js
@@ -3,8 +3,7 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  const { isLoading, hits } = useGlobalContext();
-  console.log(isLoading);
+  const { isLoading, hits, removeStory } = useGlobalContext();
 
   if (isLoading) {
     return <div className="loading"></div>;
@@ -29,7 +28,14 @@ const Stories = () => {
               >
                 read more
               </a>
-              <button className="remove-btn">remove</button>
+              <button
+                className="remove-btn"
+                onClick={() => {
+                  removeStory(objectID);
+                }}
+              >
+                remove
+              </button>
             </div>
           </article>
         );
